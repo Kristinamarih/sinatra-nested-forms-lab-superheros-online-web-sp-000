463ee091b221cf51bb86fecfd090c0010a66767b
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-08-14 09:26:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class App < Sinatra::Base
   set :views, Proc.new { File.join(root, "../views/") }
 
   get '/' do 
-    erb :'views/super_hero'
+    erb :'../views/super_hero'
   end
   
   post '/teams' do 
-    erb :team
+    
+    erb :'../views/team'
   end
   
 
